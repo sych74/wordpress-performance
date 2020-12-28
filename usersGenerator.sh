bf99da08cd516f4aa10b0a55b7cbda2adea73a30
sych74 <sych74@gmail.com>
2020-12-28 16:15:58 +0200

    Update usersGenerator.sh

--- usersGenerator.sh
+++ usersGenerator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for SW_USER_I in {1..10}; do wp --allow-root user create "username${SW_USER_I}" "username${SW_USER_I}@example.com" --user_pass="reviewsignal2021" --role="subscriber"; done;
+for SW_USER_I in {1..10}; do wp --allow-root user create "username${SW_USER_I}" "username${SW_USER_I}@example.com" --user_pass="PassForTest2021" --role="subscriber"; done;
